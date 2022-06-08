9eaa01d0bc06402faf8bfdd0f6fd1be4aa12627d
347 - Ramona Luigi <ramona.luigi@mum.or.at>
2022-06-08 10:56:33 +0200

    initialize github repository

---
+++ index.js
@@ -0,0 +1,45 @@
+//Variablen
+const Discord = require("discord.js");
+require("dotenv").config();
+const welcomeChannelId = "983669381311660032";
+const generateImage = require("./generateImage");
+
+const client = new Discord.Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS",
+  ]
+})
+
+//Event Listener
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+})
+
+client.on("messageCreate", (message) => {
+  if(message.content =="hi"){
+    message.reply("Hello World!")
+  }
+})
+
+//test für member affiliated zeug
+/*
+client.on("guildMemberUpdate", async (member) => {
+  const img = await generateImage(member);
+  member.guild.channels.cache.get(welcomeChannelId).send({
+    content: `<@${member.id}>! Welcome to the server!`,
+    files: [img]
+  })
+})
+*/
+
+client.on("guildMemberAdd", async (member) => {
+const img = await generateImage(member);
+  member.guild.channels.cache.get(welcomeChannelId).send({
+    content: `<@${member.id}>! Welcome to the server!`,
+    files: [img]
+  })
+})
+
+client.login(process.env.TOKEN);
